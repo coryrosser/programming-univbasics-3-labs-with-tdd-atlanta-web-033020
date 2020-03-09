1609b3ea281d114d0961e9033e4262eae887407e
Cortlyn Rosser <coryrosser@gmail.com>
2020-03-09 21:41:52 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,8 @@
 # Add your variables here
-first_number = gets.to_i
-second_number = gets.to_i
+first_number = ""
+second_number = ""
   
 def addition
   
-  sum = first_number ++ second_number
+  sum = first_number.to ++ second_number
 end
